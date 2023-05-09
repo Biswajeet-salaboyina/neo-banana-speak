111ce7daaa175e0c4524f99d0917c21e2047d299
Biswajeet-salaboyina <60876660+Biswajeet-salaboyina@users.noreply.github.com>
2023-05-09 10:48:04 +0530

    output

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var btnTranslate=document.querySelector("#btn-translate");
+var outputDIv=document.querySelector("#output");
+
+
 
 btnTranslate.addEventListener("click",clickHandler)
 
 function clickHandler()
 {
-    console.log("clicked") ;//call back
-    console.log("input",txtInput.value);
+    // console.log("clicked") ;//call back
+    // console.log("input",txtInput.value);
+    outputDIv.innerText="afaagagh "+ txtInput.value;
 };
 
 var txtInput=document.querySelector("#txt-input");
