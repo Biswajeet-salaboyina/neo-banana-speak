b664a5fed0c05d18b8deaa549f1c444cb39adbc4
Biswajeet-salaboyina <60876660+Biswajeet-salaboyina@users.noreply.github.com>
2023-05-09 14:51:09 +0530

    Upadate

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 var btnTranslate=document.querySelector("#btn-translate");
-var outputDIv=document.querySelector("#output");
+var outputDiv=document.querySelector("#output");
 
 
 
 btnTranslate.addEventListener("click",clickHandler)
 
+// var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL(text)
+{
+     return serverURL + "?" + "text=" +text
+}
+
+function errorhandler(error){
+    console.log("error occured", error);
+    alert("somethig went wrong")
+}
 function clickHandler()
 {
     // console.log("clicked") ;//call back
     // console.log("input",txtInput.value);
-    outputDIv.innerText="afaagagh "+ txtInput.value;
+    // outputDiv.innerText="afaagagh "+ txtInput.value;
+    var inputText=txtInput.value;
+fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then(json => {
+    var translatedText=json.contents.translated;
+    outputDiv.innerText=translatedText;
+    })
+.catch(errorhandler)
 };
+
 
 var txtInput=document.querySelector("#txt-input");
 
